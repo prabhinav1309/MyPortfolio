865cdd8c7bb0ff1de66521fd88100446226ca5db
Prabhinav Mishra <prabhi.codes@gmail.com>
2024-07-30 02:37:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-const projectIcons = document.querySelectorAll(".project-icon");
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
 
-projectIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    const project = icon.parentElement;
-    const description = project.querySelector("p");
-    description.classList.toggle("show"); // Toggle visibility of description
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
   });
-});
+}
 
 // Select the element with the typing effect
-const typingEffectElement = document.querySelector(".typing-effect");
+const typingEffectElement = document.querySelector(".text-two");
 
 // Function to reset the animation
 const resetTypingEffect = () => {
